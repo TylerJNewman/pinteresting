d9f688ac0f7a7bf6468333620fc54cd8c8d0141d
Tyler <Tylerjnewman18@gmail.com>
2015-03-31 14:41:56 -0700

    Customized and fixed resolution

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,20 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+     has_attached_file :image, :styles => {
+  :thumb    => ['100x100#',  :jpg, :quality => 70],
+  :preview  => ['480x480#',  :jpg, :quality => 70],
+  :large    => ['600>',      :jpg, :quality => 70],
+  :retina   => ['1200>',     :jpg, :quality => 30]
+},
+:convert_options => {
+  :thumb    => '-set colorspace sRGB -strip',
+  :preview  => '-set colorspace sRGB -strip',
+  :large    => '-set colorspace sRGB -strip',
+  :retina   => '-set colorspace sRGB -strip -sharpen 0x0.5'
+}
      validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
      validates :image, presence: true
      validates :description, presence: true
+
+   
 end
